25ad463d03b91dd82e72aa8bf7c8bb25de7053e7
JMG24a <jmg24a@gmail.com>
2022-03-27 13:58:37 -0400

    [UPDATE] preparando index para socket

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const appRouter = require('./router')
 const connectDB = require('./lib/mongoose')
 const app = express();
-
+const server = require('http').Server(app)
+const {connect} = require('../socket')
 app.use(express.json())
 
 connectDB()
+connect(server)
 appRouter(app)
 
 app.use('/app', express.static('public'))
 
-app.listen(3001)
+server.listen(3001,()=>{
+    console.log('[SERVER] on port 3001')
+})

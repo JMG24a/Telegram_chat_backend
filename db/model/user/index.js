d69b2c9fb31b428a0522f7298ca6eee89144b099
JMG24a <jmg24a@gmail.com>
2022-03-27 10:33:02 -0400

    [UPDATE] Modelos para un chat

---
+++ db/model/user/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const mySchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+    }
+})
+
+const model = mongoose.model('User', mySchema)
+module.exports = model;
